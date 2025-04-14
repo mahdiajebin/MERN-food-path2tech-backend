2a571d99b3d95064a0bf010b6f9b2cc0098fb5cd
mahdiajebin <mahdiajebin@gmail.com>
2025-04-13 21:20:42 -0400

    fixed Cannot set headers after they are sent to the client error

--- src/controllers/MyUserController.ts
+++ src/controllers/MyUserController.ts
@@ -1,22 +1,21 @@
-// src/controllers/MyUserController.ts
 import { Request, Response } from "express";
 import User from "../models/user";
 
-export const createCurrentUser = async (req: Request, res: Response) => {
+export const createCurrentUser = async (req: Request, res: Response): Promise<any> => {
   try {
     const { auth0Id } = req.body;
     const existingUser = await User.findOne({ auth0Id });
 
     if (existingUser) {
-       res.status(200).send();
+      return res.status(200).send();
     }
 
     const newUser = new User(req.body);
     await newUser.save();
 
-     res.status(201).json(newUser.toObject());
+    return res.status(201).json(newUser.toObject());
   } catch (error) {
     console.error(error);
-     res.status(500).json({ message: "Error creating the user" });
+    return res.status(500).json({ message: "Error creating the user" });
   }
 };
